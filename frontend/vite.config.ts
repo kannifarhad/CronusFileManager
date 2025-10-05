2e7ae84031687ca5c9f877af9867e6e530dc11fc
Farhad Aliyev <farhad@kanni.pro>
2025-10-05 20:26:52 +0300

    feat: Complete Vite Migration and cleanup code

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [["babel-plugin-react-compiler"]],
+      },
+    }),
+    svgr({
+      svgrOptions: {
+        icon: true,
       },
     }),
   ],
-})
+  server: {
+    open: true, // Opens the browser on start
+    port: 3000, // Default Vite port
+    fs: {
+      strict: false, // Allow history-based routing
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "public/index.html"),
+      },
+    },
+  },
+});
